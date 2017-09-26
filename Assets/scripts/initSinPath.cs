5d151e5e6f9d3a5bd6274cb9471b001c8d88671e
Ryan Stillings <rystills@gmail.com>
2017-09-26 03:45:42 -0400

    use camera dimensions for point selection, store points in a separate component

--- Assets/scripts/initSinPath.cs
+++ Assets/scripts/initSinPath.cs
@@ -12,12 +12,28 @@ public class initSinPath : MonoBehaviour {
 	//we will need a line renderer to draw our path
 	private LineRenderer LR;
 
+	//setup a reference to this object's points compoment, so we can insert the points produced by our sin curve
+	private pathPoints PP;
+
 	//Create a list of points which constitute a sin path with the desired fidelity and number of iterations
 	void Start () {
-		//start by initializing our line-renderer
+		//initialize our line renderer
 		LR = gameObject.AddComponent<LineRenderer>();
-		LR.SetPosition(0, new Vector3(50, 50, 50));
-		LR.SetPosition(0, new Vector3(100, 100, 100));
-		
+
+		//grab a reference to the points component
+		PP = gameObject.GetComponent<pathPoints>();
+
+		//calculate the camrea extents so we can fine-tune the path layout
+		float hHeight = Camera.main.orthographicSize;
+		float hWidth = hHeight * Screen.width / Screen.height;
+
+		//create some test points
+		PP.points.Add(new Vector3(0, 0, 0));
+		PP.points.Add(new Vector3(hWidth, hHeight, 0));
+		PP.points.Add(new Vector3(hWidth, -hHeight, 0));
+
+		//copy our points into the line renderer so that they appear on-screen
+		LR.SetVertexCount(PP.points.Count);
+		LR.SetPositions(PP.points.ToArray());		
 	}
 }
