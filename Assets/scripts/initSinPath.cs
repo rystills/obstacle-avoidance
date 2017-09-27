e368823b7852fbf29a9b520150ed69602a5c1e0b
Ryan Stillings <rystills@gmail.com>
2017-09-27 13:14:47 -0400

    add path colors, flipped path, include shader and scene in build

--- Assets/scripts/initSinPath.cs
+++ Assets/scripts/initSinPath.cs
@@ -8,6 +8,7 @@ public class initSinPath : MonoBehaviour {
 	public int numCycles;
 	public float amplitude;
 	public float period;
+	public bool flipY;
 
 	//we will need a line renderer to draw our path
 	private LineRenderer LR;
@@ -23,24 +24,23 @@ public class initSinPath : MonoBehaviour {
 		//grab a reference to the points component
 		PP = gameObject.GetComponent<pathPoints>();
 
-		//calculate the camera extents so we can fine-tune the path layout
-		float hHeight = Camera.main.orthographicSize;
-		float hWidth = hHeight * Screen.width / Screen.height;
-
 		//create the points on our sin curve (remember y = asin(b(x - c)) + d)
 		for (int r = 0; r < numCycles; ++r) {
-			for (int i = 0; i < pointsPerCycle; ++i) {
+			for (int i = 0; i < pointsPerCycle + (r == numCycles - 1 ? 1 : 0); ++i) {
 				//we can calculate x by shifting along the current period by the current point
 				float x = transform.position.x + 2 * Mathf.PI * (period * (r+i / (float)pointsPerCycle));
 				//once we have x, we plug that into our sin function to get y
-				float y = transform.position.y + amplitude * Mathf.Sin(1/period * x);
+				float y = transform.position.y + amplitude * Mathf.Sin(1/period * x) * (flipY ? -1 : 1);
 				PP.points.Add(new Vector3(x,y,0));
 			}	
 		}
 
 		//specify the lind renderer settings
-		LR.startWidth = .5f;
-		LR.endWidth = .5f;
+		LR.startWidth = .1f;
+		LR.endWidth = .1f;
+
+		LR.material = new Material(Shader.Find("Unlit/Color"));
+		LR.material.color = PP.debugColor;
 
 		//copy our points into the line renderer so that they appear on-screen
 		LR.numPositions = PP.points.Count;
--- Assets/scripts/pathPoints.cs
+++ Assets/scripts/pathPoints.cs
@@ -5,4 +5,6 @@ using UnityEngine;
 public class pathPoints : MonoBehaviour {
 	//store a list of vector3s representing this path's points; to be interfaced with from other components
 	public List<Vector3> points;
+
+	public Color debugColor;
 }
